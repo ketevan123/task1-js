0962005d6502312dadb11b70546788b3a474134d
ketevan123 <ketikevkhishvili@gmail.com>
2024-02-15 09:31:14 -0800

    finished.task

---
+++ index.js
@@ -0,0 +1,14 @@
+// დავალება1
+
+for(var index = 5; index > 0; index--) {
+    console.log(index);
+}
+
+// დავალება2
+
+ for(var i = 0; i < 10; i++ ) {
+    if(i %2 !==0) {
+        console.log(i);
+    }
+ }
+ 
